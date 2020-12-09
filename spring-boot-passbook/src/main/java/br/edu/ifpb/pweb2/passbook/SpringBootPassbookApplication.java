e738def7983f28684899d155ac3d1e01190c9147
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2020-12-09 14:28:47 -0300

    Criado projeto PassBook com Spring Boot

---
+++ spring-boot-passbook/src/main/java/br/edu/ifpb/pweb2/passbook/SpringBootPassbookApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.passbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPassbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPassbookApplication.class, args);
+	}
+
+}
---
+++ spring-boot-passbook/src/test/java/br/edu/ifpb/pweb2/passbook/SpringBootPassbookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.passbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPassbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

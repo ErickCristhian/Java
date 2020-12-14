7909b7503812f3c50b070fa7fc7b56e6145deb82
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2020-12-14 17:59:35 -0300

    Controllers e Views adicionadas

--- spring-boot-passbook/src/main/java/br/edu/ifpb/pweb2/passbook/controller/AlunoController.java
+++ spring-boot-passbook/src/main/java/br/edu/ifpb/pweb2/passbook/controller/AlunoController.java
@@ -1,5 +1,223 @@
 package br.edu.ifpb.pweb2.passbook.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.edu.ifpb.pweb2.passbook.exception.PassbookException;
+import br.edu.ifpb.pweb2.passbook.model.Aluno;
+import br.edu.ifpb.pweb2.passbook.service.AlunoService;
+
+@Controller
+@RequestMapping("/aluno")
 public class AlunoController {
+	
+	@Autowired
+	private AlunoService alunoService;
+
+	@RequestMapping("/cadastro")
+	public ModelAndView cadastro(ModelAndView modelAndView, HttpSession session) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			modelAndView.setViewName("aluno/cadastro");
+			modelAndView.addObject("aluno", new Aluno());
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+	
+	@RequestMapping("/cadastrar")
+	public ModelAndView cadastrar(ModelAndView modelAndView, HttpSession session, Aluno aluno, RedirectAttributes flash) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			modelAndView.setViewName("redirect:/aluno/cadastro");
+			try {
+				alunoService.saveAluno(aluno);
+				flash.addFlashAttribute("mensagem", "Aluno cadastrado com sucesso!");				
+			} catch (Exception e) {
+				flash.addFlashAttribute("mensagem", "Ocorreu um erro ao cadastrar o usuário!");				
+			}
+			flash.addFlashAttribute("aluno", new Aluno());
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+	@RequestMapping(value="/{id}/update")
+	public ModelAndView updateUser(@PathVariable("id") Integer id, ModelAndView modelAndView, HttpSession session) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			Aluno aluno = alunoService.findById(id).get();
+			modelAndView.setViewName("aluno/cadastro");
+			modelAndView.addObject("aluno", aluno);
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getNotas(ModelAndView modelAndView, HttpSession session) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			modelAndView.setViewName("aluno/list");
+			try {
+				List<Aluno> alunos = alunoService.findAll();
+				modelAndView.addObject("alunos", alunos);
+			} catch (PassbookException e) {
+				e.printStackTrace();
+			}
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/{id}/delete")
+	public ModelAndView deletePorId(@PathVariable("id") Integer id, ModelAndView modelAndView , RedirectAttributes attr) {
+		alunoService.deleteById(id);
+		modelAndView.setViewName("redirect:/aluno");
+		attr.addFlashAttribute("mensagem", "Aluno excluído!");
+		return modelAndView;
+	}
+
+	@RequestMapping("/{id}/notas")
+	public ModelAndView notas(@PathVariable("id") Integer id, ModelAndView modelAndView, HttpSession session) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			Aluno aluno = alunoService.findById(id).get();
+			modelAndView.setViewName("aluno/notas");
+			modelAndView.addObject("aluno", aluno);
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+
+	@RequestMapping("/{id}/atualizar-notas-aluno")
+	public ModelAndView cadastrarNotas(@PathVariable("id") Integer id,
+									   ModelAndView modelAndView, HttpSession session, Aluno alunoNotas, RedirectAttributes flash) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			modelAndView.setViewName("redirect:/aluno");
+			try {
+				Aluno aluno = alunoService.findById(id).get();
+				aluno.setNota1(alunoNotas.getNota1());
+				aluno.setNota2(alunoNotas.getNota2());
+				aluno.setNota3(alunoNotas.getNota3());
+				
+				// Calcular situação do aluno
+				if(aluno.calculaSituacao() != null) {
+					aluno.setSituacao(aluno.calculaSituacao());
+				}
+				
+				alunoService.saveAluno(aluno);
+				flash.addFlashAttribute("mensagem", "Cadastro do aluno atualizado com sucesso!");
+			} catch (Exception e) {
+				flash.addFlashAttribute("mensagem", "Ocorreu um erro atualizando o aluno!");
+			}
+			flash.addFlashAttribute("aluno", new Aluno());
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+
+	@RequestMapping("/{id}/faltas")
+	public ModelAndView faltas(@PathVariable("id") Integer id, ModelAndView modelAndView, HttpSession session) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			Aluno aluno = alunoService.findById(id).get();
+			modelAndView.setViewName("aluno/faltas");
+			modelAndView.addObject("aluno", aluno);
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+
+	@RequestMapping("/{id}/atualizar-faltas-aluno")
+	public ModelAndView cadastrarFaltas(@PathVariable("id") Integer id,
+									   ModelAndView modelAndView, HttpSession session, Aluno alunoFaltas, RedirectAttributes flash) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			modelAndView.setViewName("redirect:/aluno");
+			try {
+				Aluno aluno = alunoService.findById(id).get();
+				aluno.setFaltas(alunoFaltas.getFaltas());
+				
+				// Calcular situação do aluno
+				if(aluno.calculaSituacao() != null) {
+					aluno.setSituacao(aluno.calculaSituacao());
+				}
+				
+				alunoService.saveAluno(aluno);
+				flash.addFlashAttribute("mensagem", "Cadastro do aluno atualizado com sucesso!");
+			} catch (Exception e) {
+				flash.addFlashAttribute("mensagem", "Ocorreu um erro atualizando o aluno!");
+			}
+			flash.addFlashAttribute("aluno", new Aluno());
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+
+	@RequestMapping("/{id}/final")
+	public ModelAndView notaFinal(@PathVariable("id") Integer id, ModelAndView modelAndView, HttpSession session) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			Aluno aluno = alunoService.findById(id).get();
+			modelAndView.setViewName("aluno/final");
+			modelAndView.addObject("aluno", aluno);
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+
+	@RequestMapping("/{id}/atualizar-final-aluno")
+	public ModelAndView cadastrarNotaFinal(@PathVariable("id") Integer id,
+										ModelAndView modelAndView, HttpSession session, Aluno alunoNotaFinal, RedirectAttributes flash) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			modelAndView.setViewName("redirect:/aluno");
+			try {
+				Aluno aluno = alunoService.findById(id).get();
+				aluno.setNotaFinal(alunoNotaFinal.getNotaFinal());
+				
+				// Calcular situação final do aluno
+				if(aluno.calculaSituacaoFinal() != null) {
+					aluno.setSituacao(aluno.calculaSituacaoFinal());					
+				}
+				
+				alunoService.saveAluno(aluno);
+				flash.addFlashAttribute("mensagem", "Cadastro do aluno atualizado com sucesso!");
+			} catch (Exception e) {
+				flash.addFlashAttribute("mensagem", "Ocorreu um erro atualizando o aluno!");
+			}
+			flash.addFlashAttribute("aluno", new Aluno());
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+
+	@RequestMapping("relatorio")
+	public ModelAndView getRelatorio(ModelAndView modelAndView, HttpSession session) {
+		if (session.getAttribute("usuarioLogado") != null) {
+			modelAndView.setViewName("aluno/relatorio");
+			try {
+				List<Aluno> alunos = alunoService.findAll();
+				modelAndView.addObject("alunos", alunos);
+			} catch (PassbookException e) {
+				e.printStackTrace();
+			}
+		} else {
+			modelAndView.setViewName("redirect:/login");
+		}
+		return modelAndView;
+	}
+	
+
 
 }
--- spring-boot-passbook/src/main/java/br/edu/ifpb/pweb2/passbook/controller/UsuarioController.java
+++ spring-boot-passbook/src/main/java/br/edu/ifpb/pweb2/passbook/controller/UsuarioController.java
@@ -1,5 +1,71 @@
 package br.edu.ifpb.pweb2.passbook.controller;
 
-public class UsuarioController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.edu.ifpb.pweb2.passbook.model.Usuario;
+import br.edu.ifpb.pweb2.passbook.service.UsuarioService;
+import br.edu.ifpb.pweb2.passbook.util.PasswordUtil;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@RequestMapping("/cadastro")
+	public ModelAndView getForm(HttpSession session, ModelAndView modelAndView) {
+		modelAndView.setViewName("/usuarios/cadastro");
+		
+		Usuario usuarioLogado = (Usuario) session.getAttribute("usuarioLogado");
+		
+		if (usuarioLogado != null) {
+			Usuario usuario = usuarioService.findByLogin(usuarioLogado.getEmail()) ;
+			
+			modelAndView.addObject("usuario", usuario);
+		} else {
+			modelAndView.addObject("usuario", new Usuario());			
+		}
+		return modelAndView;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView cadastrar(Usuario usuario, ModelAndView modelAndView, HttpSession session, RedirectAttributes flash) {
+		Usuario u = usuarioService.findByLogin(usuario.getEmail());
+		if(u != null && u.getId() != usuario.getId()) {
+			flash.addFlashAttribute("mensagem", "Email já cadastrado");
+			modelAndView.setViewName("redirect:/usuarios/cadastro");
+			return modelAndView;
+		}
+		
+		modelAndView.setViewName("redirect:/login");
+		
+		String senha = PasswordUtil.hashPassword(usuario.getSenha());
+		usuario.setSenha(senha);
+		
+		String msg;
+		
+		if (usuario.getId() != null) {
+			msg = "atualiza";
+		} else {
+			msg = "cadastra";
+		}
+		
+		try {
+			usuarioService.saveUsuario(usuario);
+			flash.addFlashAttribute("mensagem", "Usuário " + msg + "do");
+		} catch (Exception e) {
+			flash.addFlashAttribute("mensagem", "Erro ao " + msg + "r usuário");
+		}
+		
+		return modelAndView;
+	}
+	
 }
